cfa1ee74a8eb8a229f00198251eb1405bf70c5ba
TAWE KELVIN <169045295+kelvintawe12@users.noreply.github.com>
2025-01-07 20:43:49 +0200

    creating the console.py file to handle pep validation

---
+++ console.py
@@ -0,0 +1,29 @@
+import cmd
+
+#!/usr/bin/python3
+"""
+Console Module
+This module contains the entry point of the command interpreter.
+"""
+
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the HBNB clone project."""
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program using EOF (Ctrl+D)."""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty input line."""
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
