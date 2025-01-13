d59d5c7beff008f50a0b9236404cb4e53511ac4b
TAWE KELVIN <169045295+kelvintawe12@users.noreply.github.com>
2025-01-13 19:23:16 +0200

    debugging the console.py file

--- console.py
+++ console.py
@@ -1,4 +1,4 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
 """
 Console Module
 This module contains the entry point of the command interpreter.
@@ -10,22 +10,27 @@ class HBNBCommand(cmd.Cmd):
     """Command interpreter for the HBNB clone project."""
     prompt = "(hbnb) "
 
-    def do_quit(self, _):
+    def do_quit(self, arg):
         """Quit command to exit the program."""
         return True
 
-    def do_EOF(self, _):
+    def do_EOF(self, arg):
         """Exit the program using EOF (Ctrl+D)."""
-        print()
+        print()  # To ensure a new line after EOF is entered
         return True
 
     def emptyline(self):
-        """Do nothing on empty input line."""
+        """Override default behavior for empty input lines."""
         pass
+
+    def help_quit(self):
+        """Provide help information for the quit command."""
+        print("Quit command to exit the program.\n")
+
+    def help_EOF(self):
+        """Provide help information for the EOF command."""
+        print("Exit the program using EOF (Ctrl+D).\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
-    print("Exiting HBNB Console...")
-    exit(0)
-else:
-    pass
