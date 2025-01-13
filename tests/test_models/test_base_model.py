6743e489d04a35e0b8f44c79ed613658bbfa74a8
Sougnabe <s.payang@alustudent.com>
2025-01-13 23:52:36 +0200

    ok

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
-'''importation'''
-from models.base_model import BaseModel
+'''importation
+from models.base_model2 import BaseModel
 
 my_model = BaseModel()
 my_model.name = "My First Model"
@@ -12,4 +12,38 @@ print(my_model_json)
 print("JSON of my_model:")
 
 for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))'''
+
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+# Create an instance of BaseModel
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+
+# Print initial instance details
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+
+print("--")
+
+# Convert instance to dictionary
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.items() or my_model_json.keys():
+    print (f"\t{key}: ({type(my_model_json[key])}) - {my_model_json[key]}")
+
+print("--")
+
+# Create a new instance from the dictionary
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+# Check if the new instance is the same as the original
+print(my_model is my_new_model)
